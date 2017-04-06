ee5d65e91fe58ac10d9bc0957cdc9dc90f6b2fd6
张福明 <zhangfuming@zhangfumingdeMacBook-Pro.local>
2017-04-06 18:59:19 +0800

    提交初始代码

--- src/server/server.go
+++ src/server/server.go
@@ -7,6 +7,10 @@ import (
 	"server/utils/logger"
 	"strings"
 	_"server/service"
+	"net/http"
+	"fmt"
+	"server/api"
+	"sync"
 )
 
 //server 启动 ./server 环境
@@ -28,6 +32,24 @@ func main() {
 		config.ENV = "DEVELOPMENT"
 	}
 	logger.Info(nil, "server start at %s", config.ENV)
+	wg := sync.WaitGroup{}
+	wg.Add(1)
+	httpServer(&wg)
+	wg.Wait()
+	logger.Info(nil,"api http server shutdown","")
+}
 
+func httpServer(wg *sync.WaitGroup){
+	defer func() {
+		if err := recover(); err != nil {
+			logger.Error(nil, "api http server error %s ", err)
+			wg.Done()
+		}
+	}()
+	http.HandleFunc("/", api.Index)
+	fmt.Println("Server is at localhost:8081")
 
+	if err := http.ListenAndServe("localhost:8081", nil); err != nil {
+		logger.Error(nil,"http server error %s",err)
+	}
 }
