fd5020c1cf75b7675d9b4db07e2eaf6936e70ae8
zhangfuming <843749178@qq.com>
2017-05-27 15:49:19 +0800

    Update config.go

--- src/server/config/config.go
+++ src/server/config/config.go
@@ -4,10 +4,10 @@ var ENV string = "DEVELOPMENT"
 
 var CONFIG = map[string]map[string]string{
 	"DEVELOPMENT": {
-		"AMQP_ADDRESS":    "amqp://admin:qpwoeiruty@192.168.19.141:5672/message",
+		"AMQP_ADDRESS":    "amqp://admin:qpwoeiruty@ip:5672/message",
 		"AMQP_QUEUE":      "smsexchange-dev",
-		"MONGO_ADDRESS":   "192.168.18.88:27017",
-		"REDIS_ADDRESS":   "192.168.18.102:6379",
+		"MONGO_ADDRESS":   "ip:27017",
+		"REDIS_ADDRESS":   "ip:6379",
 		"MAX_POOL_SIZE":   "5",
 		"REDIS_PASSWORD":  "111111",
 	},
